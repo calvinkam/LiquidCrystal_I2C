739e50e338ba90e463f87bed0a5885d04d5628f2
Calvin Kam <calvinkam123@gmail.com>
2018-10-11 16:37:05 +0800

    Change 2004 to 1602

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -1,7 +1,7 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 20 chars and 4 line display
 
 void setup()
 {
